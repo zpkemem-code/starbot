f424ab256f57bbbe9fc40ab1e0ac8bc87d4de606
ferly kurniawan <89645813+FerlyKurniawan@users.noreply.github.com>
2026-05-09 02:39:22 +0700

    Update nokos.py

--- helpers/nokos.py
+++ helpers/nokos.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import CallbackQuery
 from pyrogram.handlers import CallbackQueryHandler
-from helpers.buttons import nokos
+from .buttons import ButtonUtils
 
 
 async def cb_shop(_, callback: CallbackQuery):
-    text, button = await Buttonutils.nokos(0)
+    text, button = await ButtonUtils.nokos(0)
     return await callback.edit_message_text(
           text, reply_markup=button
     )
@@ -13,7 +13,7 @@ async def cb_shop(_, callback: CallbackQuery):
 
 async def cb_page_shop(_, callback: CallbackQuery):
     page = int(callback.matches[0].group(1))
-    text, button = await Buttonutils.nokos(page)
+    text, button = await ButtonUtils.nokos(page)
     return await callback.edit_message_text(
           text, reply_markup=button
     ) 
